cdcb1d4c4cd371d4d595aaeeb97be472916686a2
Naveen G <naveeng2402@gmail.com>
2021-10-22 18:40:58 +0530

    formatted JS

--- website/static/index.js
+++ website/static/index.js
@@ -1,9 +1,11 @@
-function delete_node(noteId){
-    console.log(noteId)
-    fetch('/delete-note', {
-        method: "POST", body: JSON.stringify({noteId: noteId})
-    },).then((_res) => {
-        window.location.href = "/";
-    });
+var HOME_URL = "/";
 
+function delete_node(noteId) {
+  console.log(noteId);
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = HOME_URL;
+  });
 }
